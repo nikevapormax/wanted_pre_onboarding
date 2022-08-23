6c5db7209f8f66ee93ff0ba5a48e3c38c182a19f
Daegeun Jeong <wjdeorms27@naver.com>
2022-08-24 03:21:37 +0900

    Test : 채용공고 수정 성공 시 테스트 코드 작성

--- companies/tests.py
+++ companies/tests.py
@@ -2,6 +2,7 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 
 from .models import Company as CompanyModel
+from .models import Recruitments as RecruitmentsModel
 class RecruitmentsTest(APITestCase):
     @classmethod
     def setUpTestData(cls):
@@ -80,3 +81,57 @@ class RecruitmentsTest(APITestCase):
         self.assertEqual(response_2.data["position"][0], "This field may not be blank.")
         self.assertEqual(response_3.data["recruit_compensation"][0], "A valid integer is required.")
         self.assertEqual(response_3.data["contents"][0], "This field may not be blank.")
+
+
+class RecruitmentsReviseOrDeleteTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        company_data = ["배달의민족", "요기요"]
+        for company in company_data:
+            cls.company = CompanyModel.objects.create(company_name=company)
+        
+        cls.company.save()
+        
+        cls.recruitments = RecruitmentsModel.objects.create(
+            company=CompanyModel.objects.get(id=1),
+            position="프론트엔드 주니어 개발자",
+            recruit_compensation=500000,
+            contents="배달의민족에서 프론트엔드 주니어 개발자를 채용합니다!",
+            skill="Vue.js"
+        )
+        cls.recruitments_1 = RecruitmentsModel.objects.create(
+            company=CompanyModel.objects.get(id=2),
+            position="백엔드 주니어 개발자",
+            recruit_compensation=1500000,
+            contents="요기요에서 백엔드 주니어 개발자를 채용합니다!",
+            skill="Python"
+        )
+        
+    # 채용공고 수정 테스트 (성공)
+    def test_revise_register(self):
+        url = reverse("companies")
+        data = {
+            "id" : 1,
+            "company" : 1,
+            "position" : "프론트엔드 시니어 개발자",
+            "recruit_compensation" : 500000,
+            "contents" : "배달의민족에서 프론트엔드 시니어 개발자를 채용합니다. 많관부!",
+            "skill" : "Vue.js",
+        }
+        data_1 = {
+            "id" : 2,
+            "company" : 2,
+            "position" : "백엔드 주니어 개발자",
+            "recruit_compensation" : 5000000000,
+            "contents" : "요기요에서 백엔드 시니어 개발자를 채용합니다. 많관부!",
+            "skill" : "Django",
+        }
+        
+        response = self.client.put(url, data)
+        response_1 = self.client.put(url, data_1)
+        
+        self.assertEqual(response.data["position"], "프론트엔드 시니어 개발자")
+        self.assertEqual(response.data["contents"], "배달의민족에서 프론트엔드 시니어 개발자를 채용합니다. 많관부!")
+        self.assertEqual(response_1.data["recruit_compensation"], 5000000000)
+        self.assertEqual(response_1.data["contents"], "요기요에서 백엔드 시니어 개발자를 채용합니다. 많관부!")
+        self.assertEqual(response_1.data["skill"], "Django")
