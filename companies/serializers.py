79d5d90768e5b05d2d90169f9c3a1b24f3ae7b9c
Daegeun Jeong <wjdeorms27@naver.com>
2022-08-24 02:21:15 +0900

    Feat : RecruitmentsChangeSerializer 구현

--- companies/serializers.py
+++ companies/serializers.py
@@ -10,3 +10,22 @@ class RecruitmentsSerializer(serializers.ModelSerializer):
         model = RecruitmentsModel
         fields = ["company", "position", "recruit_compensation", "contents", "skill"]
     
+
+class RecruitmentsChangeSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = RecruitmentsModel
+        fields = ["company", "position", "recruit_compensation", "contents", "skill"]
+    
+    def validate(self, data):
+        if data.get("company"):
+            data.pop("company")
+        print(data)
+        return data
+    
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+        
+        return instance
