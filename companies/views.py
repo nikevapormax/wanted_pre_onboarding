63bda68ea3ba33a3584bb1b7c5d38f72877ff90f
Daegeun Jeong <wjdeorms27@naver.com>
2022-08-24 02:11:01 +0900

    Feat : 채용공고 등록 기능 구현

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from reprlib import recursive_repr
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+from .models import Recruitments as RecruitmentsModel
+from .serializers import RecruitmentsSerializer
+from .serializers import RecruitmentsChangeSerializer
+from companies import serializers
+
+class CompaniesAPIView(APIView):
+    # 채용공고 등록
+    def post(self, request):
+        recruitments = RecruitmentsSerializer(data=request.data)
+
+        if recruitments.is_valid():
+            recruitments.save()
+            return Response(recruitments.data, status=status.HTTP_200_OK)
+        
+        return Response(recruitments.errors, status=status.HTTP_400_BAD_REQUEST)
+    
