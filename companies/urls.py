ebb51ca43ae37ccf4f46ee1afbc5be043d83f825
Daegeun Jeong <wjdeorms27@naver.com>
2022-08-24 16:40:35 +0900

    Feat : 쿼리 파라미터를 통한 채용공고 검색의 url 생성

--- companies/urls.py
+++ companies/urls.py
@@ -4,4 +4,5 @@ from companies import views
 
 urlpatterns = [
     path("", views.CompaniesAPIView.as_view(), name="companies"),
+    path("find/", views.RecruitmentsSearchView.as_view(), name="companies_find"),
 ]
