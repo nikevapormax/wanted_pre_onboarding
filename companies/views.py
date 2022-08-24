bbb71362094571314848453623ea84e864132bf3
Daegeun Jeong <wjdeorms27@naver.com>
2022-08-24 10:04:01 +0900

    Feat : 채용공고 삭제 기능 구현

--- companies/views.py
+++ companies/views.py
@@ -8,7 +8,7 @@ from .serializers import RecruitmentsSerializer
 from .serializers import RecruitmentsChangeSerializer
 from companies import serializers
 
-class CompaniesAPIView(APIView):
+class CompaniesAPIView(APIView):    
     # 채용공고 등록
     def post(self, request):
         recruitments = RecruitmentsSerializer(data=request.data)
@@ -29,5 +29,15 @@ class CompaniesAPIView(APIView):
             return Response(recruitments.data, status=status.HTTP_200_OK)
         
         return Response(recruitments.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    
+    # 채용공고 삭제
+    def delete(self, request):
+        try:
+            recruitments = RecruitmentsModel.objects.get(id=request.data["id"])
+            
+            if recruitments:
+                recruitments.delete()
+                return Response({"message": "채용공고 삭제 성공!"}, status=status.HTTP_200_OK)
+        except:
+            return Response({"message": "채용공고가 존재하지 않습니다."}, status=status.HTTP_404_NOT_FOUND)
         
