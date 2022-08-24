8b1ba2ba0e3928ada08813b51efaf4ebd984ebfa
Daegeun Jeong <wjdeorms27@naver.com>
2022-08-24 17:14:26 +0900

    Feat : 채용공고 상세페이지의 url 생성

--- companies/urls.py
+++ companies/urls.py
@@ -5,4 +5,5 @@ from companies import views
 urlpatterns = [
     path("", views.CompaniesAPIView.as_view(), name="companies"),
     path("find/", views.RecruitmentsSearchView.as_view(), name="companies_find"),
+    path("detail/<int:id>/", views.RecruitmentsDetailView.as_view(), name="recruitments_detail"),
 ]
